dd29aa97f013388c58b497d66c18e0e6489969d1
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-09-22 08:13:49 +0100

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,12 @@
 // Code SimplerComponent Here
+import React, {Component} from "react"
+
+const FunComponent = prop => {
+    return (
+        <div onClick={prop.handleClick}>
+            I am just happy
+        </div>
+    )
+}
+
+export default FunComponent

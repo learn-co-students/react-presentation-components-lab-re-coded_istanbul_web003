39f2d6662d47fe07dbfab4d7d8621c8fac2ecd0a
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-10-24 22:18:20 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import SimpleComponent from './components/SimpleComponent';
-import SimplerComponent from './components/SimplerComponent';
+import React from "react";
+import ReactDOM from "react-dom";
+import SimpleComponent from "./SimpleComponent";
+import SimplerComponent from "./SimplerComponent";
 
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={console.log("clicked")} />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
